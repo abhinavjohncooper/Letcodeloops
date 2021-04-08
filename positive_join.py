a42a89a80b6a5e81d87297bfb30460162e0efa79
abhinavjohncooper <81818070+abhinavjohncooper@users.noreply.github.com>
2021-04-09 00:01:05 +0530

    Update positive_join.py

--- positive_join.py
+++ positive_join.py
@@ -1,6 +1,5 @@
 list1 = [12,-7,5,64,-14]
 list2 = []
-#list2 = [12,14,-95,3] in order to check for list2 just comment out list1 and change list1 to list2 in for loop
 for number in list1:
     if number > 0:
         list2.append(str(number))    
